bc26bff8121aa5a5ec864623ac6cd6a76414b5c6
unknown <aldrin.brinas@frontlinebusiness.com.ph>
2024-09-25 10:14:11 +0800

    banner100%

--- burgir.js
+++ burgir.js
@@ -1,4 +1,6 @@
 const burgir = document.querySelector(".toggle_menu");
+const headerNav = document.querySelector(".header_nav");
 burgir.addEventListener("click", () => {
     burgir.classList.toggle("open");
+    headerNav.classList.toggle("open");
 })
